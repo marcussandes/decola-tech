0213cca0ed5f38627b6ea4c7b5fb81d1bc1952d0
Marcus Sandes <marcus.sandesf@gmail.com>
2025-03-29 11:19:35 -0300

    update arquivo

---
+++ src/main/java/me/dio/decola_dech_2025/DecolaDech2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.decola_dech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaDech2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaDech2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/decola_dech_2025/DecolaDech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decola_dech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaDech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
